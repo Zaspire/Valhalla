8f4bce70823b268494fd712bd4a4937fea1b7fa4
Maxim Ermilov <ermilov.maxim@gmail.com>
2014-11-23 21:41:20 +0300

    client: log all errors to analytics

--- client/www/common.js
+++ client/www/common.js
@@ -1,5 +1,31 @@
-var host = 'http://drunken.co:3000/';
+var host = 'http://192.168.1.8:3000/';
 
 var GA_ACCOUNT = 'UA-56813809-1';
 var SCREEN_WIDTH = 1280;
 var SCREEN_HEIGHT = 768;
+
+(function() {
+    var errors = [];
+    var ready = false;
+    function reportErrors() {
+        if (!ready)
+            return;
+        var arr = errors;
+        errors = [];
+        for (var i = 0; i < arr.length; i++) {
+            analytics.trackEvent('error', arr[i].message, arr[i].filename, arr[i].lineno);
+        }
+    }
+    window.onerror = function(message, filename, lineno) {
+        errors.push({message: message, filename: filename, lineno: lineno})
+
+        reportErrors();
+        return false;
+    }
+    document.addEventListener('deviceready', function() {
+        analytics.startTrackerWithId(GA_ACCOUNT);
+        analytics.trackView(document.title);
+        ready = true;
+        reportErrors();
+    });
+})();
