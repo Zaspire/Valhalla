986c315e4c0587f3871eeebc550618ccacc26313
Maxim Ermilov <ermilov.maxim@gmail.com>
2015-02-09 05:04:47 +0300

    game_model: fix typo

--- client/www/game_model.js
+++ client/www/game_model.js
@@ -320,6 +320,7 @@ GameStateModel.prototype = {
             break;
         case CONCEDE:
             controller.concede();
+            break;
         default:
             assert(false);
         };
